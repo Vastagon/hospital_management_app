e0ad32f3c404549d7a2b76c7af53d0df97494279
ribone <14286113+riboney@users.noreply.github.com>
2021-12-06 07:01:37 -0500

    Refactors footer out of App.js and creates Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './css/bootstrap.min.css'
 import './css/styles.css'
 import { Link, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 import LandingScreen from './Screens/LandingScreen';
 
@@ -17,22 +18,7 @@ function App() {
                 <Route path="/" element={<LandingScreen />} exact></Route>
             </Routes>
         </main>
-        <div class="copyright bg-gray">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <ul class="list-unstyled li-space-lg p-small">
-                            <li><a href="article.html">Article Details</a></li>
-                            <li><a href="terms.html">Terms & Conditions</a></li>
-                            <li><a href="privacy.html">Privacy Policy</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-6">
-                        <p class="p-small statement">Copyright © <a href="#">Your name</a></p>
-                    </div> 
-                </div> 
-            </div> 
-        </div> 
+        <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,22 @@
+export default function(props){
+    return(
+        <div>
+            <div class="copyright bg-gray">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled li-space-lg p-small">
+                                <li><a href="article.html">Article Details</a></li>
+                                <li><a href="terms.html">Terms & Conditions</a></li>
+                                <li><a href="privacy.html">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <p class="p-small statement">Copyright © <a href="#">Your name</a></p>
+                        </div> 
+                    </div> 
+                </div> 
+            </div> 
+        </div>
+    )
+}
